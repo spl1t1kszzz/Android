b83f9f5c36f213f4e9e84c6d99b76eea63544062
spl1t1kszzz <n.osmushkin@g.nsu.ru>
2024-12-04 20:27:14 +0700

    lab_1 done (i hope so)

--- lab_1/app/src/main/java/com/spl1t1kszzz/lab_1/SecondActivity.kt
+++ lab_1/app/src/main/java/com/spl1t1kszzz/lab_1/SecondActivity.kt
@@ -2,35 +2,33 @@ package com.spl1t1kszzz.lab_1
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
+        val third_activity_button = findViewById<Button>(R.id.third_activity_button)
+        third_activity_button.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
             startActivity(intent)
         }
-        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                Log.v("D", "mama")
-                finish()
-            }
-        })
-    }
 
+        val main_activity_button = findViewById<Button>(R.id.main_activity_button)
+        main_activity_button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
 }
